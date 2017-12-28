1d650d93ba336899fc10a658a53ae2e14461762d
Jonny Harris <jon@spacedmonkey.co.uk>
2017-12-28 23:27:11 +0000

    Fix object cache

--- wp-content/object-cache.php
+++ wp-content/object-cache.php
@@ -1,4 +1,4 @@
 <?php 
 require_once( WP_DROPIN_DIR . "/wp-redis/object-cache.php" );
-require_once( WP_DROPIN_DIR . "/wp-redis/cli.php" );
+require_once( WP_DROPIN_DIR . "/wp-redis/wp-redis.php" );
 
