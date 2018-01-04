0585fc4e47355d30727afbec703a41b1b2d73eb3
Jonny Harris <jon@spacedmonkey.co.uk>
2018-01-04 20:51:33 +0000

    Added a filter to disable photon with filter

--- wp-content/mu-plugins/photon.php
+++ wp-content/mu-plugins/photon.php
@@ -1,16 +1,3 @@
 <?php
 
-// do not include the opening php tag
-
-// turn Photon off so we can get the correct image
-$photon_removed = '';
-if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) { // check that we are, in fact, using Photon in the first place
-	$photon_removed = remove_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ) );
-}
-
-// do things with image functions
-
-// turn Photon back on
-if ( $photon_removed ) {
-	add_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ), 10, 3 );
-}
+add_filter( 'jetpack_photon_skip_for_url', '__return_true' );
