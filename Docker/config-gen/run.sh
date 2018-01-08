4c30a65e175d8f37e9bde4cb36dc7724a8c245ba
Jonny Harris <jon@spacedmonkey.co.uk>
2018-01-08 20:13:40 +0000

    Added new container to generate config

--- Docker/config-gen/run.sh
+++ Docker/config-gen/run.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-while read p; do
-  cp /scripts/template.conf /etc/vhosts/$p.conf
-  sed -i "s/~hostname~/$p/g" /etc/vhosts/$p.conf
-done < /etc/letsencrypt/domains.conf
-
 cp /scripts/default.conf /etc/vhosts/default.conf
+
+current_hash=
+while true; do
+   list="$(ls /etc/letsencrypt/live)"
+   new_hash="$(echo "$list" | md5sum)"
+   if [ "$current_hash" != "$new_hash" ]; then
+        for p in $list
+        do
+          cp /scripts/template.conf /etc/vhosts/$p.conf
+          sed -i "s/~hostname~/$p/g" /etc/vhosts/$p.conf
+       done
+       # Keep new hash version
+       current_hash="$new_hash"
+    fi
+   sleep 60
+done
