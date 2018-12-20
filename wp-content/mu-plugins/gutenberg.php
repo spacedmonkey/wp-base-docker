599fc4b062f24badbe322c6c67d7ff4b5223a280
Jonny Harris <spacedmonkey@users.noreply.github.com>
2018-12-20 12:24:54 +0000

    Create gutenberg.php

---
+++ wp-content/mu-plugins/gutenberg.php
@@ -0,0 +1,10 @@
+<?php
+/* Plugin Name: Disable concact in admin
+ * Description: Disable concact in admin for gutenberg
+ * Author: WordPress
+ * Version: 0.1-beta
+ */
+
+add_filter( 'js_do_concat', function(){
+   return ! is_admin();
+});
