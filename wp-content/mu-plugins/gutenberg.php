f37cc784bc77a04bc620178b1249b7d2b1a18a80
Jonny Harris <jon@spacedmonkey.co.uk>
2020-04-14 03:09:38 +0100

    Remove js

--- wp-content/mu-plugins/gutenberg.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Plugin Name: Disable concact in admin
- * Description: Disable concact in admin for gutenberg
- * Author: WordPress
- * Version: 0.1-beta
- */
-
-add_filter( 'js_do_concat', '__return_false');
